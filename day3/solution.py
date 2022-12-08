94ad4497e510f76bbe0aec3e447b421fdcd14475
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2022-12-08 22:09:40 +0530

    day3

--- day3/solution.py
+++ day3/solution.py
@@ -2,23 +2,37 @@ input = open('input.txt','r')
 
 # ASCII value of a - 97 and z - 122 => difference of 96 to obtain prio
 # ASCII value of A - 65 and Z - 90  => difference of 38 to obtain prio
-commonele = []
-total = 0
-for x in input:
-    x = [*x]
-    n = len(x)//2
-    a = x[:n-1]
-    b = x[n:]
-    
-    commonele = list(set(a) & set(b))
-    commonele.append(0)
-    commonele = commonele[0]
-    
-    if commonele != 0:
-        temp = ord(commonele)
-        if temp <90 and temp > 65:
-            total = total + temp - 38
-        elif temp >97 and temp < 122 :
-            total = total + temp - 96 
+t=0
+t2 =0
+line3 = []
+lol = []
+f=0
+for line in input:
+    line = line.strip()
+    line = [*line]
+    x = len(line) 
+    a = line[:x//2]
+    b = line[x//2:]
+    k, = set(a) & set(b)
+    if k >= "a":
+        t += ord(k) - ord("a") + 1
+    else:
+        t += ord(k) - ord("A") + 27
+    #PartB
+    line3 = line3 + line
+    lol.append(x)
+    f = f +1
+    if f == 3:
+        p = line3[:lol[0]]
+        q = line3[lol[0]:lol[0]+lol[1]]
+        r = line3[lol[0]+lol[1]:sum(lol)]
+        z,= set(p) & set(q) & set (r)
+        if z >= "a":
+            t2 += ord(z) - ord("a") + 1
+        else:
+            t2 += ord(z) - ord("A") + 27
+        f = 0 
+        line3.clear()
+        lol.clear()
 
-print(total)
+print(" Part A :",t,'\n','Part B :',t2)
